26df6e788b196845a033afe3736161dcf7d59153
Amit Maharjan <maharjanamit8@gmail.com>
2024-07-04 12:15:21 -0400

    Style Changed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-    $('#mobile-menu').click(function() {
-        $('#navbar-links').slideToggle();
+$(document).ready(function () {
+    // Toggle mobile menu
+    $('#mobile-menu').click(function () {
+        $(this).toggleClass('open');
+        $('#navbar-links').toggleClass('show');
+    });
+
+    // Smooth scroll for navigation links
+    $('.navbar-links a').click(function (e) {
+        e.preventDefault();
+        const target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 50
+        }, 1000);
+        $('#navbar-links').removeClass('show');
+        $('#mobile-menu').removeClass('open');
     });
 });
