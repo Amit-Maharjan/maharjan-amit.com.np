50e46dcd28a46d3ed4017f565d3476c760662338
Amit Maharjan <maharjanamit8@gmail.com>
2025-12-09 10:51:47 -0500

    Changed the layout design of the website

--- scripts.js
+++ scripts.js
@@ -1,18 +1,53 @@
 $(document).ready(function () {
     // Toggle mobile menu
-    $('#mobile-menu').click(function () {
+    $('#mobile-menu').on('click', function () {
         $(this).toggleClass('open');
         $('#navbar-links').toggleClass('show');
     });
 
     // Smooth scroll for navigation links
-    $('.navbar-links a').click(function (e) {
-        e.preventDefault();
+    $('.nav-link').on('click', function (e) {
         const target = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top - 50
-        }, 1000);
-        $('#navbar-links').removeClass('show');
-        $('#mobile-menu').removeClass('open');
+        if (target && target.startsWith('#')) {
+            e.preventDefault();
+            const $targetEl = $(target);
+            if ($targetEl.length) {
+                $('html, body').animate(
+                    { scrollTop: $targetEl.offset().top - 80 },
+                    600
+                );
+            }
+            $('#navbar-links').removeClass('show');
+            $('#mobile-menu').removeClass('open');
+        }
     });
+
+    // Scrollspy-style active nav + navbar shrink
+    const sections = $('section');
+    const navLinks = $('.nav-link');
+
+    function onScroll() {
+        const scrollPos = $(document).scrollTop();
+        const offsetBuffer = 140;
+
+        sections.each(function () {
+            const top = $(this).offset().top - offsetBuffer;
+            const bottom = top + $(this).outerHeight();
+            const id = $(this).attr('id');
+
+            if (scrollPos >= top && scrollPos < bottom) {
+                navLinks.removeClass('active');
+                $('.nav-link[href="#' + id + '"]').addClass('active');
+            }
+        });
+
+        if (scrollPos > 40) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    }
+
+    $(document).on('scroll', onScroll);
+    onScroll();
 });
